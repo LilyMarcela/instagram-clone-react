d583db9b891036bebaf3e15afa3e100d15513e84
lily <marcela.b.abril@gmail.com>
2018-09-24 20:35:02 -0500

    add graphql server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Post from './components/Post';
+    // src/App.js
 
+    import React, { Component } from 'react';
+    import './App.css';
+    import Header from './components/Header';
+    import Post from './components/Post';
 
-class App extends Component {
-  render() {
-    return (
+    class App extends Component {
+      render() {
+        return <div className="App">
+            <Header />
+            <section className="App-main">
+              <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+              <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
 
-      <div> 
-        <Header />
-        <div>
-          <Post/>
-        </div>
-      </div>
+              {/* more posts */}
+            </section>
+          </div>;
+      }
+    }
 
-
-    );
-  }
-}
-
-export default App;
+    export default App;
